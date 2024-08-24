4e6eca570d7c2c84fc72b2b12658af92680e218b
jovan <git.keseljevicjovan@proton.me>
2024-08-24 16:36:51 +0200

    Make tmux config, add nvim-tmux-navigator

--- .config/nvim/lua/keseljevicjovan/core/remap.lua
+++ .config/nvim/lua/keseljevicjovan/core/remap.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = " "
 
+-- Panes
+vim.keymap.set("n", "<C-k>", ':wincmd k<CR>')
+vim.keymap.set("n", "<C-j>", ':wincmd j<CR>')
+vim.keymap.set("n", "<C-h>", ':wincmd h<CR>')
+vim.keymap.set("n", "<C-l>", ':wincmd l<CR>')
+
 -- Project
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-q>pv", vim.cmd.Ex)
